4bf4b79d258793d59f653d2e46a144e8785c17b4
saudsaeed1992-eng <saudsaeed1992@gmail.com>
2026-05-26 13:58:53 +0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,10 @@
 export const metadata = {
   title: 'PANDO APP',
   description: '2-Month Transformation Dashboard',
+  icons: {
+    icon: '/pando_app_cartoon_icon.png',
+    apple: '/pando_app_cartoon_icon.png',
+  },
 }
 
 export default function RootLayout({ children }) {
